821bc2cb680c6fa4b7cd0bb6bc5ba368719aa3b6
Eigil Sagafos <eigil@shiftx.com>
2025-03-24 06:57:30 -0700

    valdres: don't save save mutations on rootTxn

--- packages/@roc-db/valdres/src/functions/end.ts
+++ packages/@roc-db/valdres/src/functions/end.ts
@@ -1,7 +1,9 @@
 import type { ValdresTransaction } from "../types/ValdresTransaction"
 
 export const end = (txn: ValdresTransaction) => {
-    txn.engineOpts.rootTxn.commit()
+    // txn.engineOpts.rootTxn.commit()
+    // WARNING: We treat the valdres adapter as a optimistic adapter, so we don't save the mutations on the root store
+    txn.engineOpts.txn.commit()
     if (
         txn.engineOpts.scopedStore &&
         txn.engineOpts.scopedStoreAlreadyAttachedBeforeBegin
--- packages/@roc-db/valdres/src/functions/saveMutation.ts
+++ packages/@roc-db/valdres/src/functions/saveMutation.ts
@@ -1,6 +1,9 @@
 export const saveMutation = (txn, finalizedMutation) => {
     const { mutationAtom } = txn.engineOpts
     const atom = mutationAtom(txn.mutation.ref)
-    txn.engineOpts.rootTxn.set(atom, finalizedMutation)
+    // WARNING: We treat the valdres adapter as a optimistic adapter, so we don't save the mutations on the root store
+    // TODO: We should ensure that the mutations are saved on the root store when applying the change set
+    // txn.engineOpts.rootTxn.set(atom, finalizedMutation)
+    txn.engineOpts.txn.set(atom, finalizedMutation)
     return finalizedMutation
 }
