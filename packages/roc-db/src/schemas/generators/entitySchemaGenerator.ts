9191841d3e981a823919584cd19ae89afcec919e
Eigil Sagafos <eigil@shiftx.com>
2025-04-09 15:34:15 -0700

    Fix entitySchemaGenerator types

--- packages/roc-db/src/schemas/generators/entitySchemaGenerator.ts
+++ packages/roc-db/src/schemas/generators/entitySchemaGenerator.ts
@@ -1,29 +1,38 @@
 import { z, type ZodObject } from "zod"
-import { EntitySchema } from "../EntitySchema"
 import { refSchemaGenerator } from "./refSchemaGenerator"
-import { Ref } from "../../types/Ref"
+import { MutationRefSchema } from "../MutationRefSchema"
+
+const TimestampWithMutationRefSchema = z.object({
+    mutationRef: MutationRefSchema,
+    timestamp: z.string().datetime({ precision: 3 }),
+})
 
 export const entitySchemaGenerator = <
     const Entity extends string,
-    Data extends ZodObject<any> = ZodObject<{}>,
-    Children extends ZodObject<any> = ZodObject<{}>,
-    Parents extends ZodObject<any> = ZodObject<{}>,
-    Ancestors extends ZodObject<any> = ZodObject<{}>,
+    const Data extends ZodObject<any> = ZodObject<{}>,
+    const Children extends ZodObject<any> = ZodObject<{}>,
+    const Parents extends ZodObject<any> = ZodObject<{}>,
+    const Ancestors extends ZodObject<any> = ZodObject<{}>,
 >(
     entity: Entity,
     args: {
-        ref?: Ref
         data?: Data
         children?: Children
         parents?: Parents
         ancestors?: Ancestors
-    } = {},
-) =>
-    EntitySchema.extend({
-        ref: args.ref ?? refSchemaGenerator(entity),
-        entity: z.literal(entity),
-        data: (args.data ?? z.object({})).strict() as Data,
-        children: (args.children ?? z.object({})).strict() as Children,
-        parents: (args.parents ?? z.object({})).strict() as Parents,
-        ancestors: (args.ancestors ?? z.object({})).strict() as Ancestors,
-    })
+    },
+) => {
+    return z
+        .object({
+            ref: refSchemaGenerator(entity),
+            entity: z.string(),
+            created: TimestampWithMutationRefSchema.required().strict(),
+            updated: TimestampWithMutationRefSchema.required().strict(),
+            data: (args.data ?? z.object({}).strict()) as Data,
+            children: (args.children ?? z.object({}).strict()) as Children,
+            parents: (args.parents ?? z.object({}).strict()) as Parents,
+            ancestors: (args.ancestors ?? z.object({}).strict()) as Ancestors,
+        })
+        .strict()
+        .required()
+}
