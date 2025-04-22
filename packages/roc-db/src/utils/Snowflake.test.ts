e301248b3237b08d7fc98aa349bab121bade864f
Eigil Sagafos <eigil@shiftx.com>
2025-04-21 19:24:25 -0700

    Add checks to verify correct snowflake groupId and serverId usage. Add tests

--- packages/roc-db/src/utils/Snowflake.test.ts
+++ packages/roc-db/src/utils/Snowflake.test.ts
@@ -12,4 +12,76 @@ describe("Snowflake", () => {
         expect(res1).toStrictEqual([ts, 1, 1, 0])
         expect(res2).toStrictEqual([ts, 1, 1, 1])
     })
+
+    test("group over 256 fails", () => {
+        expect(() => new Snowflake(256, 1)).toThrow(
+            "groupId is 8 bit so must be between 0 and 255",
+        )
+    })
+    test("group under 0 fails", () => {
+        expect(() => new Snowflake(-1, 1)).toThrow(
+            "groupId is 8 bit so must be between 0 and 255",
+        )
+    })
+    test("serverId over 16383 fails", () => {
+        expect(() => new Snowflake(0, 16384)).toThrow(
+            "serverId is 14 bit so must be between 0 and 16,383",
+        )
+    })
+    test("serverId under 0 fails", () => {
+        expect(() => new Snowflake(0, -1)).toThrow(
+            "serverId is 14 bit so must be between 0 and 16,383",
+        )
+    })
+
+    test("timestamp before epoch fails", () => {
+        const snowflake = new Snowflake(0, 0)
+        const date = new Date("2023-10-01T00:00:00Z")
+        expect(() => snowflake.generate(Number(date))).toThrowError(
+            "Timestamp is before epoch",
+        )
+    })
+
+    test("generating over 1024 ids in the same millisecond fails", () => {
+        const snowflake = new Snowflake(0, 0)
+        const date = new Date()
+        date.setMilliseconds(date.getMilliseconds() + 1)
+        const ids = []
+        for (let i = 0; i < 4096; i++) {
+            ids.push(snowflake.generate(Number(date)))
+        }
+        expect(ids[0]).toBeString()
+        expect(ids[4095]).toBeString()
+        expect(ids[4096]).toBeUndefined()
+        expect(ids[0]).not.toEqual(ids[4095])
+        expect(() => snowflake.generate(Number(date))).toThrowError(
+            "Error! Sequence overflow",
+        )
+    })
+
+    test("date 100 years in the future parses correctly", () => {
+        const date = new Date()
+        date.setFullYear(date.getFullYear() + 100)
+        const snowflake1 = new Snowflake(0, 0)
+        const snowflake2 = new Snowflake(255, 16383)
+        const id1 = snowflake1.generate(Number(date))
+        const id2 = snowflake2.generate(Number(date))
+        const parsed1 = snowflake1.parse(id1)
+        const parsed2 = snowflake2.parse(id2)
+        expect(parsed1).toEqual([Number(date), 0, 0, 0])
+        expect(parsed2).toEqual([Number(date), 255, 16383, 0])
+    })
+
+    test("date 1000 years in the future parses correctly", () => {
+        const date = new Date()
+        date.setFullYear(date.getFullYear() + 1000)
+        const snowflake1 = new Snowflake(0, 0)
+        const snowflake2 = new Snowflake(255, 16383)
+        const id1 = snowflake1.generate(Number(date))
+        const id2 = snowflake2.generate(Number(date))
+        const parsed1 = snowflake1.parse(id1)
+        const parsed2 = snowflake2.parse(id2)
+        expect(parsed1).toEqual([Number(date), 0, 0, 0])
+        expect(parsed2).toEqual([Number(date), 255, 16383, 0])
+    })
 })
--- packages/roc-db/src/utils/Snowflake.ts
+++ packages/roc-db/src/utils/Snowflake.ts
@@ -1,6 +1,8 @@
 const debugLog = []
 const IDSET = new Set<string>()
 
+const DEFAULT_EPOCH = 1717225200000 // July 1 2024
+
 export class Snowflake {
     private groupId: number
     private serverId: number
@@ -8,14 +10,24 @@ export class Snowflake {
     private lastTimestamp: number
     private epoch: number
 
-    constructor(groupId: number, serverId: number) {
+    constructor(
+        groupId: number,
+        serverId: number,
+        epoch: number = DEFAULT_EPOCH,
+    ) {
         if (groupId === undefined) throw new Error("groupId is required")
         if (serverId === undefined) throw new Error("serverId is required")
+        if (groupId > 255 || groupId < 0)
+            throw new Error("groupId is 8 bit so must be between 0 and 255")
+        if (serverId > 16_383 || serverId < 0)
+            throw new Error(
+                "serverId is 14 bit so must be between 0 and 16,383",
+            )
         this.groupId = groupId
         this.serverId = serverId
         this.sequence = 0
         this.lastTimestamp = -1
-        this.epoch = 1717225200000 // July 1 2024
+        this.epoch = epoch
     }
 
     public generate(currentTimestamp = Date.now()): string {
@@ -27,11 +39,11 @@ export class Snowflake {
 
             // this.sequence
             if (this.sequence === 0) {
-                // Wait for next millisecond
-                throw new Error("TDOPD")
-                while (currentTimestamp <= this.lastTimestamp) {
-                    currentTimestamp = Date.now()
-                }
+                //TODO: Wait for next millisecond
+                throw new Error("Error! Sequence overflow")
+                // while (currentTimestamp <= this.lastTimestamp) {
+                //     currentTimestamp = Date.now()
+                // }
             }
         } else {
             // debugLog.push({
@@ -44,22 +56,19 @@ export class Snowflake {
 
         this.lastTimestamp = currentTimestamp
 
-        // Construct the ID:
-        // - 41 bits for time in milliseconds
-        // - 13 bits for server id
-        // - 10 bits for sequence number
-
         // timestamp in milliseconds
-        // 8 bit group (2^8 - 256) 128 - intern/128 - realtime
-        // 14 bit server/user (2^14 - 16,364)
+        // 8 bit group (2^8 - 0-255) 128 - intern/128 - realtime
+        // 14 bit server/user (2^14 - 0-16,383)
         // 12 bit sequence (2^12 - 4,096)
         // |8bit|14bit|12bit
-        // 1234718234712834 | 1 | 1 | 0
-        // 1234718234712834 | 0-127 | 1 | 0
+        // 1234718234712834 | 1     | 1       | 0
+        // 1234718234712834 | 0-127 | 0-16383 | 0-1023
 
         // 00000000000000000000000000000000000000000000000000000000000000000
+        const seconds = currentTimestamp - this.epoch
+        if (seconds < 0) throw new Error("Timestamp is before epoch")
         const id = (
-            (BigInt(currentTimestamp - this.epoch) << 34n) |
+            (BigInt(seconds) << 34n) |
             (BigInt(this.groupId) << 26n) |
             (BigInt(this.serverId) << 12n) |
             BigInt(this.sequence)
