f7e2e925f4663c1a3ed56c5e0418d1db00a0825a
Eigil Sagafos <eigil@shiftx.com>
2025-10-01 16:52:17 -0400

    Fix TS on refSchemaGenerator

--- packages/roc-db/src/schemas/generators/refSchemaGenerator.ts
+++ packages/roc-db/src/schemas/generators/refSchemaGenerator.ts
@@ -1,18 +1,21 @@
 import * as z from "zod"
 
+const GENERIC = z.templateLiteral([
+    z.string().min(1, "Entity kind cannot be empty"),
+    z.literal("/"),
+    z.number().int(),
+])
+
 export const refSchemaGenerator = <const Entities extends string[]>(
     ...entityKinds: Entities
 ) => {
     if (entityKinds.length === 0) {
-        return z.templateLiteral([
-            z.string().min(1, "Entity kind cannot be empty"),
-            z.literal("/"),
-            z.number().int(),
-        ])
+        return GENERIC as Entities["length"] extends 0 ? typeof GENERIC : never
     }
-    return z.templateLiteral([
+    const specific = z.templateLiteral([
         z.enum(entityKinds, { error: "Invalid entity kind" }),
         z.literal("/"),
         z.number().int(),
     ])
+    return specific
 }
