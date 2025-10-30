cae14abcb0d3d4553207edaba6204d2ecedd15c9
Eigil Sagafos <eigil@shiftx.com>
2025-10-29 22:00:54 -0700

    Re-use scopeStore in valdres beginRequest if possible

--- packages/@roc-db/valdres/src/functions/beginRequest.ts
+++ packages/@roc-db/valdres/src/functions/beginRequest.ts
@@ -2,6 +2,17 @@ import type { WriteRequest } from "roc-db"
 import type { ValdresEngine } from "../types/ValdresEngine"
 import { generateTransactionCache } from "../../../../roc-db/src/lib/generateTransactionCache"
 
+const selectScopedStore = (engineOpts: ValdresEngine, changeSetRef) => {
+    if (
+        engineOpts.scopedStore &&
+        changeSetRef in engineOpts.store.data.scopes
+    ) {
+        return engineOpts.scopedStore
+    } else {
+        return engineOpts.store.scope(changeSetRef)
+    }
+}
+
 export const beginRequest = (
     request: WriteRequest,
     engineOpts: ValdresEngine,
@@ -10,8 +21,11 @@ export const beginRequest = (
     if (request.changeSetRef) {
         if (engineOpts.store) {
             const scopedStoreAlreadyAttachedBeforeBegin =
-                !!engineOpts.store.data.scopes[request.changeSetRef]
-            const scopedStore = engineOpts.store.scope(request.changeSetRef)
+                !!engineOpts.scopedStore
+            const scopedStore = selectScopedStore(
+                engineOpts,
+                request.changeSetRef,
+            )
             return engineOpts.rootTxn.scope(request.changeSetRef, scopedTxn => {
                 if (scopedStoreAlreadyAttachedBeforeBegin) {
                     scopedTxn.data.txnCache ||= generateTransactionCache(false)
