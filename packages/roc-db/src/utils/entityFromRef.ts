8d2fa367ebc0aef6270d752c3165efd11c8bc8f8
Eigil Sagafos <eigil.sagafos@ardoq.com>
2025-11-07 09:18:28 -0800

    Improve typing of entityFromRef

--- packages/roc-db/src/utils/entityFromRef.ts
+++ packages/roc-db/src/utils/entityFromRef.ts
@@ -1,2 +1,5 @@
-export const entityFromRef = (ref: string): string =>
-    ref?.substring(0, ref?.indexOf("/"))
+type ExtractEntityFromRef<T extends `${string}/${string}`> =
+    T extends `${infer Entity}/${string}` ? Entity : never
+
+export const entityFromRef = <T extends `${string}/${string}`>(ref: T) =>
+    ref?.substring(0, ref?.indexOf("/")) as ExtractEntityFromRef<T>
